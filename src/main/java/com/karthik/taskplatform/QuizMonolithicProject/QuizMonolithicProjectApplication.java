f0e1682703475b772a98fef56ae402691faec4c6
Karthikakkireddy <karthikakkireddy5@gmail.com>
2026-02-03 18:06:38 +0530

    Initial commit and postgesql connection configuration

---
+++ src/main/java/com/karthik/taskplatform/QuizMonolithicProject/QuizMonolithicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.karthik.taskplatform.QuizMonolithicProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMonolithicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizMonolithicProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karthik/taskplatform/QuizMonolithicProject/QuizMonolithicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthik.taskplatform.QuizMonolithicProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMonolithicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
