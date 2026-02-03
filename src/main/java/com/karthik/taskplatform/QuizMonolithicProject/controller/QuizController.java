88493129166a7d99b8fed40758118e8c8f9dd509
Karthikakkireddy <karthikakkireddy5@gmail.com>
2026-02-03 21:07:10 +0530

    Add get questions api using question DTO

--- src/main/java/com/karthik/taskplatform/QuizMonolithicProject/controller/QuizController.java
+++ src/main/java/com/karthik/taskplatform/QuizMonolithicProject/controller/QuizController.java
@@ -2,16 +2,16 @@ package com.karthik.taskplatform.QuizMonolithicProject.controller;
 
 
 import com.karthik.taskplatform.QuizMonolithicProject.domain.Quiz;
+import com.karthik.taskplatform.QuizMonolithicProject.dto.QuestionResponseDTO;
 import com.karthik.taskplatform.QuizMonolithicProject.service.QuizService;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.service.annotation.PostExchange;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -27,5 +27,11 @@ public class QuizController
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 quizService.createQuiz(category, numQ, title)
         );
+    }
+
+    @GetMapping("/getQuestions/{id}")
+    public ResponseEntity<List<QuestionResponseDTO>> getQuestions(@PathVariable int id)
+    {
+        return ResponseEntity.ok(quizService.getQuestions(id));
     }
 }
--- src/main/java/com/karthik/taskplatform/QuizMonolithicProject/service/QuizService.java
+++ src/main/java/com/karthik/taskplatform/QuizMonolithicProject/service/QuizService.java
@@ -2,12 +2,14 @@ package com.karthik.taskplatform.QuizMonolithicProject.service;
 
 import com.karthik.taskplatform.QuizMonolithicProject.domain.Questions;
 import com.karthik.taskplatform.QuizMonolithicProject.domain.Quiz;
+import com.karthik.taskplatform.QuizMonolithicProject.dto.QuestionResponseDTO;
 import com.karthik.taskplatform.QuizMonolithicProject.repository.QuestionRepo;
 import com.karthik.taskplatform.QuizMonolithicProject.repository.QuizRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.awt.desktop.QuitEvent;
 import java.util.List;
 
 @Service
@@ -25,5 +27,30 @@ public class QuizService
         quiz.setQuestions(questionsList);
 
         return  quizRepo.save(quiz);
+    }
+
+    public List<QuestionResponseDTO> getQuestions(int id)
+    {
+        Quiz quiz = quizRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("ID NOT FOUND " + id));
+        List<Questions> questionsList = quiz.getQuestions();
+
+        List<QuestionResponseDTO> questionResponseDTOS = questionsList.stream()
+                .map(this::mapToQuestionResponse)
+                .toList();
+
+        return questionResponseDTOS;
+
+    }
+
+    private QuestionResponseDTO mapToQuestionResponse(Questions questions)
+    {
+        QuestionResponseDTO questionResponseDTO = new QuestionResponseDTO();
+        questionResponseDTO.setQuestionTitle(questions.getQuestionTitle());
+        questionResponseDTO.setOption1(questions.getOption1());
+        questionResponseDTO.setOption2(questions.getOption2());
+        questionResponseDTO.setOption3(questions.getOption3());
+        questionResponseDTO.setOption4(questions.getOption4());
+
+        return questionResponseDTO;
     }
 }
